93f2f4b527e3b0fc687a76e0ce576a0681b2e4d8
Aleksander Klyuev <klyuevaleks@gmail.com>
2023-04-23 01:40:56 +0500

    Добавлен репозиторий.

--- src/main/java/ru/tester/ping/dao/repository/TestResultRepository.java
+++ src/main/java/ru/tester/ping/dao/repository/TestResultRepository.java
@@ -1,10 +1,29 @@
 package ru.tester.ping.dao.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import ru.tester.ping.dao.entity.TestResult;
 
-import java.util.List;
+import java.time.LocalDateTime;
 
-public interface TestResultRepository extends CrudRepository<TestResult, Long> {
-    List<TestResult> findAll();
+public interface TestResultRepository extends JpaRepository<TestResult, Long> {
+
+    Page<TestResult> findAllByHostOrderByExecuteDateDesc(String host, Pageable pageable);
+    Page<TestResult> findAllByExecuteDateBetweenOrderByExecuteDateDesc(LocalDateTime startDate, LocalDateTime endDate, Pageable pageable);
+
+/*    @Query(value = "select tr from Test_Result tr where 1=1 \n" +
+                "    and ( :host is null or tr.host = :host ) \n" +
+                "    and ( :startDate is null or tr.execute_Date > :startDate ) \n" +
+                "    and ( :endDate is null or tr.execute_Date < :endDate ) \n" +
+                "    order by tr.execute_Date desc \n"
+            , countQuery = "select count(tr) from TestResult tr where 1=1 \n" +
+            "    and ( :host is null or tr.host = :host ) \n" +
+            "    and ( :startDate is null or tr.execute_Date > :startDate ) \n" +
+            "    and ( :endDate is null or tr.execute_Date < :endDate ) \n"
+            , nativeQuery = true)
+     Page<TestResult> getTestResult(@Param("host") String host,
+                                                    @Param("startDate") LocalDateTime startDate,
+                                                    @Param("endDate") LocalDateTime endDate,
+                                                    Pageable pageable);*/
 }
